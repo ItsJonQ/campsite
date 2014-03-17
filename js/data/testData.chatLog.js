eae492ad047c0c540793119c6f9744460266797d
ItsJonQ <itsjonq@gmail.com>
2014-03-17 12:31:55 -0400

    Added data to campfire chat

--- js/data/testData.chatLog.js
+++ js/data/testData.chatLog.js
@@ -6,7 +6,82 @@
             "user": "Jon Q",
             "message": "yoyoyo",
             "timestamp": "9:15 AM"
+        },
+
+        {
+            "user": "Chase C",
+            "message": "yo!",
+            "timestamp": ""
+        },
+
+        {
+            "user": "David H",
+            "message": "sup sup",
+            "timestamp": ""
+        },
+
+        {
+            "user": "Wailin W",
+            "message": "mooooornin!",
+            "timestamp": "9:30 AM"
+        },
+
+        {
+            "user": "Nick Q",
+            "message": "Decided to ImageOptim all of my photo backups on Dropbox. Roughly 10% savings. Not sure if worth it.",
+            "timestamp": ""
+        },
+
+        {
+            "user": "Jon Q",
+            "message": "Hmmm.. Prolly?",
+            "timestamp": ""
+        },
+
+        {
+            "user": "Tony G",
+            "message": "Didn't upgrade to Windows 8 until now because I thought I'd hate it..... Guess what? I hate it.",
+            "timestamp": "9:35 AM"
+        },
+
+        {
+            "user": "Jon Q",
+            "message": "I tried it back in Nov last year for a bit. Could not get use to it what so ever. Drove me nuts",
+            "timestamp": ""
+        },
+
+        {
+            "user": "Tony G",
+            "message": "no start menu, apps on different screens, unintuitive interface, no productivity. OS for touch screens forced onto desktops.",
+            "timestamp": ""
+        },
+
+        {
+            "user": "Jamis B",
+            "message": "Feeling awful today, so I'm baking bread. I still feel awful, but in a few hours, at least I'll have fresh-baked bread.",
+            "timestamp": ""
+        },
+
+        {
+            "user": "Matthew K",
+            "message": "Mmmmmm...",
+            "timestamp": ""
+        },
+
+        {
+            "user": "Kristin A",
+            "message": "MMMMMM",
+            "timestamp": ""
+        },
+
+        {
+            "user": "Jon Q",
+            "message": "MMMMMMHMMM!",
+            "timestamp": ""
         }
+
+
+
 
     ]
 }
