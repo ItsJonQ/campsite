704be6596cc7131592fa8a67ee015ce6f4e29297
ItsJonQ <itsjonq@gmail.com>
2014-03-17 03:12:20 -0400

    First Commit :D

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
+
+        browserify: {
+            main: {
+                src: 'js/app.js',
+                dest: 'public/js/main.js'
+            }
+        },
+
+        jshint: {
+            files: ['js/**/*.js'],
+            options: {
+                force: true,
+                strict: true,
+                smarttabs: true,
+                globals: {
+                    _: true,
+                    ko: true,
+                    jQuery: true,
+                    console: true,
+                    module: true
+                }
+            }
+        },
+
+        watch: {
+            scripts: {
+                files: ['js/**/*.js'],
+                tasks: ['browserify:main']
+            }
+        }
+
+    });
+
+
+    // Loading Grunt Tasks
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browserify');
+
+
+    // Tasks
+    grunt.registerTask('default', ['browserify', 'watch']);
+
+    grunt.registerTask('bund', ['jshint', 'browserify']);
+
+};
