266b3703417f87340d42e2fa171fad7b312d954f
ItsJonQ <itsjonq@gmail.com>
2014-03-17 16:40:06 -0400

    Refactored the fetch util

--- js/utils/utils.fetch.js
+++ js/utils/utils.fetch.js
@@ -1,6 +1,9 @@
 // Fetch
 // Fetch will be used to get fake data to populate the Basecamp dash
 
+// Defining the default ajax method
+var ajax;
+
 // Defining fetching of To-Do
 var toDo;
 
@@ -13,24 +16,41 @@ var chatBot;
 
 // Fetch will use the jQuery .ajax method to retrieve data
 
-toDo = function(callback) {
+ajax = function(url, callback) {
 
-    // Defining the URl to fetch from
-    var url = 'js/data/testData.toDo.js';
+    // Return false if url is not defined
+    if(!url) return false;
 
     // Return the $.ajax method
     return $.ajax({
         dataType: 'json',
         url: url,
+        error: function() {
+            return console.log("There was a problem!");
+        },
         success: function(data) {
 
             // Return the callback func if defined
             if(callback && typeof callback === 'function') {
-                // Returning with data
+                // Returning with data with callback
                 return callback(data);
             }
+
+            // Return the data
+            return data;
+
         }
     });
+
+};
+
+toDo = function(callback) {
+
+    // Defining the URl to fetch from
+    var url = 'js/data/testData.toDo.js';
+
+    // Returning the ajax call
+    return ajax(url, callback);
 
 };
 
@@ -40,19 +60,8 @@ comments = function(callback) {
     // Defining the URl to fetch from
     var url = 'js/data/testData.discussion.js';
 
-    // Return the $.ajax method
-    return $.ajax({
-        dataType: 'json',
-        url: url,
-        success: function(data) {
-
-            // Return the callback func if defined
-            if(callback && typeof callback === 'function') {
-                // Returning with data
-                return callback(data);
-            }
-        }
-    });
+    // Returning the ajax call
+    return ajax(url, callback);
 
 };
 
@@ -62,19 +71,8 @@ chatLog = function(callback) {
     // Defining the URl to fetch from
     var url = 'js/data/testData.chatLog.js';
 
-    // Return the $.ajax method
-    return $.ajax({
-        dataType: 'json',
-        url: url,
-        success: function(data) {
-
-            // Return the callback func if defined
-            if(callback && typeof callback === 'function') {
-                // Returning with data
-                return callback(data);
-            }
-        }
-    });
+    // Returning the ajax call
+    return ajax(url, callback);
 
 };
 
@@ -83,24 +81,14 @@ chatBot = function(callback) {
     // Defining the URl to fetch from
     var url = 'js/data/testData.chatBot.js';
 
-    // Return the $.ajax method
-    return $.ajax({
-        dataType: 'json',
-        url: url,
-        success: function(data) {
-
-            // Return the callback func if defined
-            if(callback && typeof callback === 'function') {
-                // Returning with data
-                return callback(data);
-            }
-        }
-    });
+    // Returning the ajax call
+    return ajax(url, callback);
 
 };
 
 // Exporting the fetch API
 module.exports = {
+    ajax: ajax,
     comments: comments,
     chatLog: chatLog,
     chatBot: chatBot,
--- public/js/main.js
+++ public/js/main.js
@@ -753,6 +753,9 @@ module.exports = init;
 // Fetch
 // Fetch will be used to get fake data to populate the Basecamp dash
 
+// Defining the default ajax method
+var ajax;
+
 // Defining fetching of To-Do
 var toDo;
 
@@ -765,24 +768,41 @@ var chatBot;
 
 // Fetch will use the jQuery .ajax method to retrieve data
 
-toDo = function(callback) {
+ajax = function(url, callback) {
 
-    // Defining the URl to fetch from
-    var url = 'js/data/testData.toDo.js';
+    // Return false if url is not defined
+    if(!url) return false;
 
     // Return the $.ajax method
     return $.ajax({
         dataType: 'json',
         url: url,
+        error: function() {
+            return console.log("There was a problem!");
+        },
         success: function(data) {
 
             // Return the callback func if defined
             if(callback && typeof callback === 'function') {
-                // Returning with data
+                // Returning with data with callback
                 return callback(data);
             }
+
+            // Return the data
+            return data;
+
         }
     });
+
+};
+
+toDo = function(callback) {
+
+    // Defining the URl to fetch from
+    var url = 'js/data/testData.toDo.js';
+
+    // Returning the ajax call
+    return ajax(url, callback);
 
 };
 
@@ -792,19 +812,8 @@ comments = function(callback) {
     // Defining the URl to fetch from
     var url = 'js/data/testData.discussion.js';
 
-    // Return the $.ajax method
-    return $.ajax({
-        dataType: 'json',
-        url: url,
-        success: function(data) {
-
-            // Return the callback func if defined
-            if(callback && typeof callback === 'function') {
-                // Returning with data
-                return callback(data);
-            }
-        }
-    });
+    // Returning the ajax call
+    return ajax(url, callback);
 
 };
 
@@ -814,19 +823,8 @@ chatLog = function(callback) {
     // Defining the URl to fetch from
     var url = 'js/data/testData.chatLog.js';
 
-    // Return the $.ajax method
-    return $.ajax({
-        dataType: 'json',
-        url: url,
-        success: function(data) {
-
-            // Return the callback func if defined
-            if(callback && typeof callback === 'function') {
-                // Returning with data
-                return callback(data);
-            }
-        }
-    });
+    // Returning the ajax call
+    return ajax(url, callback);
 
 };
 
@@ -835,24 +833,14 @@ chatBot = function(callback) {
     // Defining the URl to fetch from
     var url = 'js/data/testData.chatBot.js';
 
-    // Return the $.ajax method
-    return $.ajax({
-        dataType: 'json',
-        url: url,
-        success: function(data) {
-
-            // Return the callback func if defined
-            if(callback && typeof callback === 'function') {
-                // Returning with data
-                return callback(data);
-            }
-        }
-    });
+    // Returning the ajax call
+    return ajax(url, callback);
 
 };
 
 // Exporting the fetch API
 module.exports = {
+    ajax: ajax,
     comments: comments,
     chatLog: chatLog,
     chatBot: chatBot,
